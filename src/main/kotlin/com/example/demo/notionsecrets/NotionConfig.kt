6358a92d226b57ddbe75e73b4ead9541ad8051d0
JeremyPflipsen <jeremypflipsen@gmail.com>
2023-05-25 14:52:27 -0500

    adding quote integration

--- src/main/kotlin/com/example/demo/notionsecrets/NotionConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.example.demo.notionsecrets
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.stereotype.Component
-
-@ConfigurationProperties("notion")
-data class NotionConfig(val developer_id: String, val key_id: String, val signing_secret: String)
--- src/main/kotlin/com/example/demo/service/DoorDashService.kt
+++ src/main/kotlin/com/example/demo/service/DoorDashService.kt
@@ -24,15 +24,62 @@ import java.util.*
 @GrpcService
 class DoorDashService(var appProperties: AppProperties) : DoorDashServiceGrpcKt.DoorDashServiceCoroutineImplBase() {
 
-    val jwt: String = createJwt();
+    var jwt: String = this.createJwt();
 
     override suspend fun makeAndCall(request: EmptyInput): Delivery {
-        println(appProperties.developer_id)
         val createDeliveryResponse: DeliveryStatus = createDelivery(jwt);
 
         val deliveryStatus : DeliveryStatus = getDeliveryStatus(jwt, createDeliveryResponse.externalDeliveryId?: "")
 
         return delivery { hello = Gson().toJson(deliveryStatus) }
+    }
+
+    override suspend fun quotes(request: EmptyInput): Delivery {
+        val deliveryStatus:DeliveryStatus = getQuote();
+        return delivery { hello = Gson().toJson(deliveryStatus)}
+    }
+
+//    override suspend fun manyQuotes(request: EmptyInput): Delivery {
+//        val deliveryStatus:DeliveryStatus = getQuote();
+//        return delivery { hello = Gson().toJson(deliveryStatus)}
+//    }
+
+    fun getQuote(): DeliveryStatus {
+        val body = """{
+                        "external_delivery_id": "${UUID.randomUUID()}",
+                        "pickup_address": "901 Market Street 6th Floor San Francisco, CA 94103",
+                        "pickup_business_name": "Wells Fargo SF Downtown",
+                        "pickup_phone_number": "+16505555555",
+                        "pickup_instructions": "Enter gate code 1234 on the callbox.",
+                        "dropoff_address": "901 Market Street 6th Floor San Francisco, CA 94103",
+                        "dropoff_business_name": "Wells Fargo SF Downtown",
+                        "dropoff_phone_number": "+16505555555",
+                        "dropoff_instructions": "Enter gate code 1234 on the callbox.",
+                        "order_value": 1999
+                    }"""
+
+        val client = HttpClient.newBuilder().build();
+        val request = HttpRequest.newBuilder().uri(URI.create("https://openapi.doordash.com/drive/v2/quotes/"))
+            .header("Authorization", "Bearer $jwt").header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(body)).build();
+
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        println(response.body())
+
+        return Gson().fromJson(response.body(), DeliveryStatus::class.java);
+    }
+
+    fun acceptQuote(externalDeliveryId:String): DeliveryStatus{
+        val body = """"""
+        val client = HttpClient.newBuilder().build();
+        val request = HttpRequest.newBuilder().uri(URI.create("https://openapi.doordash.com/drive/v2/quotes/$externalDeliveryId/accept"))
+            .header("Authorization", "Bearer $jwt").header("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(body)).build();
+
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        println(response.body())
+
+        return Gson().fromJson(response.body(), DeliveryStatus::class.java);
     }
 
     fun createJwt(): String {
